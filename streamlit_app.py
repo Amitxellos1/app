e1b49b9c4e4c0b6193ba20cd8ed56aa3e36a3ef4
Amitxellos1 <161604759+Amitxellos1@users.noreply.github.com>
2025-07-14 21:12:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ st.set_page_config(page_title="Logs Manager", layout="wide")
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Define Logs", "Search Logs", "Version Control", "Import/Export"])
 
-# if page == "Define Logs":
-#     define_logs.show()
-# elif page == "Search Logs":
-#     search_logs.show()
-# elif page == "Version Control":
-#     version_control.show()
-# elif page == "Import/Export":
-#     import_export.show()
+if page == "Define Logs":
+    define_logs.show()
+elif page == "Search Logs":
+    search_logs.show()
+elif page == "Version Control":
+    version_control.show()
+elif page == "Import/Export":
+    import_export.show()
